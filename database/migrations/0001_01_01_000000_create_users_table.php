edc2c14bbffbea491d8940207b3dfefbb36a0dc9
Matias Celentano <matiascelentano562@gmail.com>
2026-07-24 16:17:33 -0300

    feat: configurando base de datos y creando migraciones.

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -14,9 +14,12 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('surname');
+            $table->string('username')->unique();
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->date('date_of_birth')->nullable();
             $table->string('password');
+            $table->enum('role', ['cliente', 'admin'])->default('cliente');
             $table->rememberToken();
             $table->timestamps();
         });
